869cd45c46a68cb71fa2b36ce161cbd63cc9b288
Douglas Nogueira <douglaswnogueira@hotmail.com>
2023-09-23 18:46:05 -0300

    Created personalized exception

--- src/main/java/br/com/dougnog/ecommerce/services/UserService.java
+++ src/main/java/br/com/dougnog/ecommerce/services/UserService.java
@@ -2,6 +2,7 @@ package br.com.dougnog.ecommerce.services;
 
 import br.com.dougnog.ecommerce.entities.User;
 import br.com.dougnog.ecommerce.repository.UserRepository;
+import br.com.dougnog.ecommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User inser(User user) {
