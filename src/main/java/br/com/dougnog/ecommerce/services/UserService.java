4bf1ba040eccc607de944edd0800149ab111320a
Douglas Nogueira <douglaswnogueira@hotmail.com>
2023-09-23 18:57:34 -0300

    update method updated

--- src/main/java/br/com/dougnog/ecommerce/services/UserService.java
+++ src/main/java/br/com/dougnog/ecommerce/services/UserService.java
@@ -4,6 +4,7 @@ import br.com.dougnog.ecommerce.entities.User;
 import br.com.dougnog.ecommerce.repository.UserRepository;
 import br.com.dougnog.ecommerce.services.exceptions.DatabaseException;
 import br.com.dougnog.ecommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
