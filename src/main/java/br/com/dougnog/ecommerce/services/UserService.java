e61ba02fa41c5e2deed1bf7457a3894f4246e1d3
Douglas Nogueira <douglaswnogueira@hotmail.com>
2023-09-23 18:54:32 -0300

    Created personalized exception for delete

--- src/main/java/br/com/dougnog/ecommerce/services/UserService.java
+++ src/main/java/br/com/dougnog/ecommerce/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.dougnog.ecommerce.services;
 
 import br.com.dougnog.ecommerce.entities.User;
 import br.com.dougnog.ecommerce.repository.UserRepository;
+import br.com.dougnog.ecommerce.services.exceptions.DatabaseException;
 import br.com.dougnog.ecommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
